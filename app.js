386ed5210778f698d13ff89383326b4dbeb91933
FPLCodes <59481822+FPLCodes@users.noreply.github.com>
2020-01-30 18:25:19 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const result = document.getElementById("text");
 const fire = document.getElementById("f");
 const water = document.getElementById("w");
 const grass = document.getElementById("g");
+const userSmall = "(You)".fontsize(4).sub();
+const compSmall = "(Trainer)".fontsize(4).sub();
 
 function word(letter) {
   return letter === "f" ? "Fire" : letter === "w" ? "Water" : "Grass";
@@ -14,7 +16,7 @@ function word(letter) {
 
 function getCompChoice() {
     const choices = ['f', 'w' ,'g'];
-    const randNum = Math.floor(Math.random() * 3)
+    const randNum = Math.floor(Math.random() * 3);
     return choices[randNum];
 }
 
@@ -22,7 +24,8 @@ function win(userChoice, compChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
     compScore_span.innerHTML = compScore;
-    result.innerHTML = word(userChoice) + " beats " + word(compChoice) + " You win!";
+    result.innerHTML = `${word(userChoice)}${userSmall} beats ${word(compChoice)}${compSmall} You win!`;
+    document.getElementById(userChoice).classList.add("correct");
     if (userChoice === "f") {
       result.style.color="red";
     }
@@ -32,13 +35,15 @@ function win(userChoice, compChoice) {
     if (userChoice === "w") {
       result.style.color = "blue";
     }
+  setTimeout(function () {document.getElementById(userChoice).classList.remove('correct')}, 400);
 }
 
 function lose(userChoice, compChoice) {
   compScore++;
   compScore_span.innerHTML = compScore;
   userScore_span.innerHTML = userScore;
-  result.innerHTML = word(compChoice) + " beats " + word(userChoice) + " You lose!";
+  result.innerHTML = `${word(compChoice)}${compSmall} beats ${word(userChoice)}${userSmall} You lose!`;
+  document.getElementById(userChoice).classList.add("wrong");
   if (userChoice === "f") {
     result.style.color = "blue";
   }
@@ -46,12 +51,15 @@ function lose(userChoice, compChoice) {
     result.style.color = "red";
   }
   if (userChoice === "w") {
-    result.style.color = "green";
+    result.style.color = "darkgreen";
   }
+  setTimeout(function () { document.getElementById(userChoice).classList.remove('wrong') }, 400);
 }
 
 function draw(userChoice, compChoice) {
   result.innerHTML = "It's a draw";
+  document.getElementById(userChoice).classList.add("draw");
+  setTimeout(function () { document.getElementById(userChoice).classList.remove('draw') }, 400);
 }
 
 function game(userChoice) {
